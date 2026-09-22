28609b6f10a7f3973234a3f5f8012db8f69de76e
Chathumi <chathumirash234@gmail.com>
2026-09-22 13:52:02 +0530

    Remove standalone chathumi-module folder - fully integrated into app/Controllers and app/Views

--- chathumi-module/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/config/session.php';
-require_once __DIR__ . '/core/Auth.php';
-
-$auth = new Auth();
-header('Location: ' . ($auth->isLoggedIn() ? '/views/dashboard.php' : '/login.php'));
-exit;
--- chathumi-module/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/config/session.php';
-require_once __DIR__ . '/core/Auth.php';
-
-$auth = new Auth();
-$auth->logout();
-header('Location: /login.php');
-exit;
